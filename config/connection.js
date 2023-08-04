ca612eb582dfee105bfd20e2c79ee8e0bde0f877
EgRoku <jimenezjj5150@gmail.com>
2023-08-04 00:26:26 -0700

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
+require('dotenv').config();
+
 const Sequelize = require('sequelize');
-
-
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  // ennv file was not working so put in my incredibly encrpyted password for mysql
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, 'password', {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
